3e695bd246bda224b5113b6b59926eaa57c9c294
TAKEKAWA Takashi <takekawa@tk2lab.org>
2021-08-04 18:18:28 +0000

    simplified

--- logbesselk/bessel.py
+++ logbesselk/bessel.py
@@ -5,27 +5,25 @@ from .utils import get_deriv_func, find_zero
 from .math import abs, sign, log, cosh, log_sinh, log_cosh, log_sum_exp
 
 
-def log_bessel_k(v, x, m=100, tol_dt=1e-5, tol=None, max_iter=10):
+def log_bessel_k(v, x):
     def deriv0(t):
         return log_cosh(v * t) - x * cosh(t)
-    return _log_bessel_common(v, x, deriv0, m, tol_dt, tol, max_iter)
+    return _log_bessel_common(v, x, deriv0)
 
 
-def log_bessel_dkdv(v, x, m=100, tol_dt=1e-5, tol=None, max_iter=10):
+def log_bessel_dkdv(v, x):
     def deriv0(t):
         return log(t) + log_sinh(v * t) - x * cosh(t)
-    return sign(v) * _log_bessel_common(
-        abs(v), x, deriv0, m, tol_dt, tol, max_iter,
-    )
+    return sign(v) * _log_bessel_common(abs(v), x, deriv0)
 
 
-def log_bessel_dkdx(v, x, m=100, tol_dt=1e-5, tol=None, max_iter=10):
+def log_bessel_dkdx(v, x):
     def deriv0(t):
         return log_cosh(v * t) + log_cosh(t) - x * cosh(t)
-    return _log_bessel_common(v, x, deriv0, m, tol_dt, tol, max_iter)
+    return _log_bessel_common(v, x, deriv0)
 
 
-def _log_bessel_common(v, x, deriv0, m, tol_dt, tol, max_iter):
+def _log_bessel_common(v, x, deriv0):
 
     def derivth(t):
         return deriv0(t) - th
@@ -33,12 +31,10 @@ def _log_bessel_common(v, x, deriv0, m, tol_dt, tol, max_iter):
     shape = tf.shape(v * x)
     dtype = (v * x).dtype
 
-    if tol is None:
-        tol = np.finfo(dtype.as_numpy_dtype).eps
-
-    tol_dt = tf.constant(tol_dt, dtype)
-    tol = tf.constant(tol, dtype)
-    m = tf.constant(m)
+    tol = tf.constant(np.finfo(dtype.as_numpy_dtype).eps, dtype)
+    tol_dt = tf.constant(1e-5, dtype)
+    max_iter = 100
+    bins = 256
 
     zero = tf.zeros(shape, dtype)
     one = tf.ones(shape, dtype)
@@ -46,15 +42,11 @@ def _log_bessel_common(v, x, deriv0, m, tol_dt, tol, max_iter):
     deriv1 = get_deriv_func(deriv0)
     deriv2 = get_deriv_func(deriv1)
 
-    t0 = tf.fill(shape, tol_dt)
-    dt = tf.where(deriv1(t0) > 0, one, zero) 
-    tp = find_zero(deriv1, t0, dt, tol_dt, max_iter)
+    dt = tf.where((deriv1(zero) > 0) | (deriv2(zero) > 0), one, zero) 
+    tp = find_zero(deriv1, zero, dt, tol_dt, max_iter)
     th = deriv0(tp) + log(tol)
-
-    dt = tf.where(derivth(zero) < 0, tp, zero)
-    ts = find_zero(derivth, dt, -dt, tol_dt, max_iter)
     te = find_zero(derivth, tp, one, tol_dt, max_iter)
 
-    h = (te - ts) / tf.cast(m, dtype)
-    t = tf.linspace(ts + 0.5 * h, te - 0.5 * h, m)
+    h = te / bins
+    t = tf.linspace(0.5 * h, te - 0.5 * h, bins)
     return log_sum_exp(deriv0(t), axis=0) + log(h)
--- logbesselk/utils.py
+++ logbesselk/utils.py
@@ -2,52 +2,47 @@ import tensorflow as tf
 
 
 def get_deriv_func(func):
-    def deriv(t):
+    def deriv(x):
         with tf.GradientTape() as g:
-            g.watch(t)
-            d = func(t)
-        return g.gradient(d, t)
+            g.watch(x)
+            f = func(x)
+        return g.gradient(f, x)
     return deriv
 
 
 def find_zero(func, x, dx, tol, max_iter):
 
-    def cond(x0, x1, diff):
+    def cond(x0, x1, f0, f1, diff):
         return tf.reduce_any(tf.abs(diff) > tol)
 
-    def body(x0, x1, diff):
-        with tf.GradientTape() as g0:
-            g0.watch(x0)
-            f0 = func(x0)
-        d0 = g0.gradient(f0, x0)
-        with tf.GradientTape() as g1:
-            g1.watch(x1)
-            f1 = func(x1)
-        d1 = g1.gradient(f1, x1)
-
-        deriv = (f1 - f0) / (x1 - x0)
-        newton = tf.math.is_finite(f0) & (tf.math.abs(deriv / d0) > 1)
-        x_newton = tf.where(newton, x0 - f0 / d0, x1 - f1 / d1)
-        f_newton = func(x_newton)
-
+    def body(x0, x1, f0, f1, diff):
+        x_newton = x0 - f0 / deriv(x0)
         x_shrink = x0 + (x1 - x0) / 2
+        x_extend = x0 + (x1 - x0) * 3
+
+        f_newton = func(x_newton)
         f_shrink = func(x_shrink)
+        f_extend = func(x_extend)
 
         dn = (x_newton - x0) / (x1 - x0)
-        shrink = (dn < 0) | (1 < dn) | (tf.math.abs(f_newton / f_shrink) > 1)
+        shrink = tf.math.is_nan(x_newton) | (dn < 0) | (1 < dn)
+        shrink |= (tf.math.abs(f_shrink / f_newton) < 1)
         x0_new = tf.where(shrink, x_shrink, x_newton)
         f0_new = tf.where(shrink, f_shrink, f_newton)
-        x1_new = tf.where(f0_new * f0 > 0, x1, x0)
+        x1_new = tf.where(f0 * f0_new < 0, x0, x1)
+        f1_new = tf.where(f0 * f0_new < 0, f0, f1)
         
-        extend = (f0 * f1 > 0.0)        
-        x0_new = tf.where(extend, x1, x0_new)
-        x1_new = tf.where(extend, x0 + (x1 - x0) * 3, x1_new)
+        x0_new = tf.where(f0 * f1 <= 0, x0_new, x1)
+        f0_new = tf.where(f0 * f1 <= 0, f0_new, f1)
+        x1_new = tf.where(f0 * f1 <= 0, x1_new, x_extend)
+        f1_new = tf.where(f0 * f1 <= 0, f1_new, f_extend)
         
-        finish = x0 == x1
-        x0_new = tf.where(finish, x0, x0_new)
-        x1_new = tf.where(finish, x1, x1_new)
-        return x0_new, x1_new, tf.math.abs(x0_new - x0)
-    
-    return tf.while_loop(
-        cond, body, body(x, x + dx, 10 * tol), maximum_iterations=max_iter,
-    )[0]
+        x0_new = tf.where(x0 != x1, x0_new, x0)
+        f0_new = tf.where(x0 != x1, f0_new, f0)
+        x1_new = tf.where(x0 != x1, x1_new, x1)
+        f1_new = tf.where(x0 != x1, f1_new, f1)
+        return x0_new, x1_new, f0_new, f1_new, tf.math.abs(x0_new - x0)
+
+    deriv = get_deriv_func(func)
+    init = body(x, x + dx, func(x), func(x + dx), 10 * tol)
+    return tf.while_loop(cond, body, init, maximum_iterations=max_iter)[0]
