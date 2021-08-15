fec10aa11f9fea0d262fc0c353d66cfaa4575a6a
TAKEKAWA Takashi <takekawa@tk2lab.org>
2021-08-15 15:26:32 +0000

    refactoring

--- logbesselk/cf2.py
+++ logbesselk/cf2.py
@@ -19,28 +19,26 @@ def _log_bessel_ku(v, x, mask=None):
     Computer Physics Communications, 47, 245-257 (1987).
     """
 
-    def cond(si, ri, i, ai, bi, gi, hi, qi, qj, ci, ti):
-        nonzero_update = tk.abs(hi * ti) > tol * tk.abs(si)
+    def cond(si, ri, i, bi, ci, di, fp, fi, gi, hi):
+        nonzero_update = tk.abs(di * hi) > tol * tk.abs(si)
         if mask is not None:
             nonzero_update &= mask
         return tf.reduce_any(nonzero_update)
 
-    def body(si, ri, i, ai, bi, gi, hi, qm, qi, ci, ti):
+    def body(si, ri, i, bi, ci, di, fp, fi, gi, hi):
         j = i + 1.
+        aj = tf.square(i + 0.5) - tf.square(u)
+        bj = 2. * (x + j)
 
-        aj = ai + 2. * j
-        bj = bi + 2.
+        cj = 1. / (bj - aj * ci)
+        dj = di * (bj * cj - 1.)
+        rj = ri + dj
 
-        gj = 1. / (bj - ai * gi)
-        hj = hi * (bj * gj - 1.)
-
-        qj = (bi * qi - qm) / ai
-        cj = ci * ai / j
-        tj = ti + cj * qj
-
-        sj = si + hj * tj
-        rj = ri + hj
-        return sj, rj, j, aj, bj, gj, hj, qi, qj, cj, tj
+        fj = (bi * fi - fp) / aj
+        gj = gi * aj / j
+        hj = hi + fj * gj
+        sj = si + dj * hj
+        return sj, rj, j, bj, cj, dj, fi, fj, gj, hj
 
     max_iter = 100
 
@@ -52,24 +50,23 @@ def _log_bessel_ku(v, x, mask=None):
     tol = tk.epsilon(x.dtype)
     n = tk.round(v)
     u = v - n
-    a0 = 0.25 - tk.square(u)
-    b0 = 2. * x
-    s0, r0 = 1., 0.
 
     i = tf.cast(1., x.dtype)
-    a1 = a0 + 2.
-    b1 = b0 + 2.
-    g1 = 1. / b1
-    h1 = 1. / b1
-    q0 = tf.zeros_like(v * x, x.dtype)
-    q1 = tf.ones_like(v * x, x.dtype)
-    c1 = a0
-    t1 = a0
-    s1 = s0 + h1 * t1
-    r1 = r0 + h1
-    init = s1, r1, i, a1, b1, g1, h1, q0, q1, c1, t1
+    a1 = 0.25 - tf.square(u)
+    b1 = 2. * x + 2.
 
+    c1 = 1. / b1
+    d1 = a1 * c1
+    r1 = d1
+
+    f0 = tf.zeros_like(v * x)
+    f1 = tf.ones_like(v * x)
+    g1 = tf.ones_like(v * x)
+    h1 = 0. + f1 * g1
+    s1 = 1. + d1 * h1
+
+    init = s1, r1, i, b1, c1, d1, f0, f1, g1, h1
     sn, rn, *_ = tf.while_loop(cond, body, init, maximum_iterations=max_iter)
-    log_ku = 0.5 * tk.log(tk.pi / b0) - x - tk.log(sn)
-    log_kup1 = log_ku + tk.log((0.5 + u + x - a0 * rn) / x)
+    log_ku = 0.5 * tk.log(0.5 * tk.pi / x) - x - tk.log(sn)
+    log_kup1 = log_ku + tk.log((0.5 + u + x - rn) / x)
     return log_ku, log_kup1
