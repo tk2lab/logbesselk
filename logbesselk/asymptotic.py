fc6dfa66875dab535059d235afc4792c0373a994
TAKEKAWA Takashi <takekawa@tk2lab.org>
2021-08-21 05:48:37 +0000

    fix bug

--- logbesselk/asymptotic.py
+++ logbesselk/asymptotic.py
@@ -6,10 +6,10 @@ from .utils import wrap_log_k
 
 @wrap_log_k
 def log_bessel_k(v, x):
-    return _log_bessel_k(v, x)[0]
+    return _log_bessel_k(v, x)
 
 
-def _log_bessel_k(v, x, mask=None, max_iter=100):
+def _log_bessel_k(v, x, mask=None, max_iter=30, return_counter=False):
     """
     Digital Library of Mathematical Functions: https://dlmf.nist.gov/10.41
     """
@@ -25,22 +25,23 @@ def _log_bessel_k(v, x, mask=None, max_iter=100):
         next_fac = tf.tensor_scatter_nd_add(next_fac, [[j], [j + 1]], [u0, u1])
         return ui, next_fac, j - 1, i, factor
 
-    def cond(target, update, factor, i, pi):
-        update = update == i
+    def cond(target, counter, factor, i, pi):
+        update = counter == i
         if mask is not None:
             update &= mask
         return tf.reduce_any(update)
 
-    def body(target, update, factor, i, pi):
+    def body(target, counter, factor, i, pi):
         ui = tf.zeros_like(v * x)
         next_fac = tf.zeros_like(factor)
         init = ui, next_fac, i, i, factor
-        ui, factor = tf.while_loop(local_cond, local_body, init)[:2]
+        ui, factor, *_ = tf.while_loop(local_cond, local_body, init)
         diff = ui / pi
-        converge = tk.log(tk.abs(diff)) <= tk.log(tol) + tf.abs(const)
-        target = tf.where(converge, target + diff, target)
-        update = tf.where(converge, update, update + 1)
-        return target, update, factor, i + 1, pi * p
+        nonzerodiff = tk.abs(diff) > tol * tk.abs(target)
+        update = (counter == i) & nonzerodiff
+        target = tf.where(update, target + diff, target)
+        counter = tf.where(update, counter + 1, counter)
+        return target, counter, factor, i + 1, pi * p
 
     x = tf.convert_to_tensor(x)
     v = tf.convert_to_tensor(v, x.dtype)
@@ -51,11 +52,18 @@ def _log_bessel_k(v, x, mask=None, max_iter=100):
     p = tk.sqrt(tk.square(v) + tk.square(x))
     q = tk.square(v) / (tk.square(v) + tk.square(x))
 
-    const = 0.5 * tk.log(0.5 * tk.pi / p) + v * tk.log((v + p) / x) - p
+    log_const = 0.5 * tk.log(0.5 * tk.pi / p) + v * tk.log((v + p) / x) - p
     target = tf.zeros_like(v * x)
-    update = tf.zeros_like(v * x, tf.int32)
+    counter = tf.zeros_like(v * x, tf.int32)
     factor = tf.ones((max_iter,), x.dtype)
     i = tf.cast(0, tf.int32)
     pi = tf.ones_like(p)
-    init = target, update, factor, i, pi
-    return tf.while_loop(cond, body, init, maximum_iterations=max_iter)[:2]
+    init = target, counter, factor, i, pi
+    target, counter, *_ = tf.while_loop(
+        cond, body, init, maximum_iterations=max_iter,
+    )
+    results = log_const + tk.log(target)
+
+    if return_counter:
+        return results, counter
+    return results
