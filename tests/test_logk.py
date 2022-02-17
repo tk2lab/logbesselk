7ed2c6401417facfa61269614a9db97b7106af5a
TAKEKAWA Takashi <takekawa@tk2lab.org>
2022-02-17 13:06:35 +0000

    Add tests and fix bugs.

--- tests/test_logk.py
+++ tests/test_logk.py
@@ -4,6 +4,7 @@ import numpy as np
 
 from logbesselk.integral import log_bessel_k
 from logbesselk.integral import bessel_ke
+from logbesselk.integral import bessel_k_ratio
 
 
 def test_logk(datadir):
@@ -28,3 +29,20 @@ def test_logdkdx(datadir):
     x = df['x']
     val = df['true']
     assert np.all(np.isclose(log_bessel_k(v, x, 0, 1).numpy(), val))
+
+
+def test_ke(datadir):
+    df = pd.read_csv('./data/ke_mathematica.csv')
+    v = df['v']
+    x = df['x']
+    val = df['true']
+    assert np.all(np.isclose(bessel_ke(v, x).numpy(), val))
+
+
+def test_ke(datadir):
+    df = pd.read_csv('./data/kratio_mathematica.csv')
+    v = df['v']
+    x = df['x']
+    val = np.array(df['true'])
+    calc = bessel_k_ratio(v, x).numpy()
+    assert np.all(np.isclose(calc, val))
