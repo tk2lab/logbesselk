2399dc4d89e8ad6480c0c5175a2db8411d42850a
TAKEKAWA Takashi <takekawa@tk2lab.org>
2023-01-22 02:09:13 +0900

    Add simple evaluation for jax

---
+++ eval/eval_jax.py
@@ -0,0 +1,29 @@
+import functools
+import os
+os.environ["JAX_ENABLE_X64"] = "True"
+
+import numpy as np
+import pandas as pd
+import jax
+import jax.numpy as jnp
+from logbesselk.jax.integral import log_abs_deriv_bessel_k
+
+logk = jax.jit(jax.vmap(log_abs_deriv_bessel_k))
+
+dtype = jnp.float64
+eps = jnp.finfo(dtype).eps
+df = pd.read_csv("eval/data/logk_mathematica.csv")
+v = jnp.asarray(df.v, dtype)
+x = jnp.asarray(df.x, dtype)
+res = logk(v, x).block_until_ready()
+df["res"] = np.asarray(res)
+df["condition"] = np.square(df.v) / df.x
+df["diff"] = df.res.astype(np.float64) - df.mathematica
+df["rdiff"] = (df.res.astype(np.float64) - df.mathematica) / df.mathematica
+df["absrdiff"] = np.abs(df.rdiff)
+print("all case")
+print(df[["diff", "rdiff", "absrdiff"]].describe())
+df.sort_values("condition", inplace=True)
+print("v^2/x < 100 case")
+print(df.query("condition < 100")[["diff", "rdiff", "absrdiff"]].describe())
+df.to_csv("eval/data/logk_int_64.csv")
